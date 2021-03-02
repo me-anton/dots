6952089fd983d919c654ecca0b0e1391d2f8c6c0
Anton Korzh <antonkrzh@outlook.com>
2021-03-02 21:32:37 +0200

    Add a script for turning off the screen on idle

---
+++ scripts/autodim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Only exported variables can be used within the timer's command.
+export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
+
+# Run xidlehook
+xidlehook \
+  `# Don't lock when there's a fullscreen application` \
+  `# --not-when-fullscreen` \
+  `# Don't lock when there's audio playing` \
+  --not-when-audio \
+  `# Dim the screen after 10 minutes, undim if user becomes active` \
+  --timer 600 \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness .2' \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+  `# turn off screen and undim after 2 more seconds` \
+  --timer 30 \
+    'xset dpms force off; xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+    ''
